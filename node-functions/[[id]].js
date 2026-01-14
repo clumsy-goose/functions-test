98b5a232d4144278d60bab517e1732b770d81221
corbinlin <corbinlin@tencent.com>
2026-01-14 15:27:06 +0800

    test3

--- node-functions/[[id]].js
+++ node-functions/[[id]].js
@@ -1,4 +1,4 @@
 export async function onRequest(context) {
     console.log("🚀 ~ onRequest ~ context:", context);
-    return new Response("Hello, world! from Hello World Cloud Function[id] 动态路由 ");
+    return new Response("Hello, world! from Hello World Cloud Function[[id]] 动态路由 ");
 }
