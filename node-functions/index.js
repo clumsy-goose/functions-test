aa42579aa4e201f82d686dd89fa54b15cd028035
corbinlin <corbinlin@tencent.com>
2026-01-14 15:20:00 +0800

    test2

---
+++ node-functions/index.js
@@ -0,0 +1,4 @@
+export async function onRequest(context) {
+    console.log("🚀 ~ onRequest ~ context:", context);
+    return new Response("Hello, world! from Hello World Cloud Function index ");
+}
